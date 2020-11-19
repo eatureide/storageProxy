6b4132e39a41bb0ab6fe9d4b93f3cb1746837782
Eature <781746820@qq.com>
2020-11-19 18:47:16 +0800

    ll

--- src/tool.js
+++ src/tool.js
@@ -1,10 +1,29 @@
-const handler = {
-  get(target, property) {
-    return Reflect.get(target, property)
-  },
-  set(_, property) {
-    return Reflect.set(target, property)
+class Storage {
+
+  constructor () {
+    this.storage = localStorage
+  }
+
+  getItem(key) {
+    const res = JSON.parse(localStorage.getItem(key) || '{}')
+    return localStorage.getItem(res.value)
+  }
+
+  setItem(key, value) {
+
+    const transfer = JSON.stringify({
+      value,
+      key,
+      type: typeof value
+    })
+
+    const { value } = JSON.parse(localStorage.getItem(key) || '{}')
+
+    localStorage.setItem(key, transfer)
   }
 }
 
-localStorage.__proto__ = new Proxy(localStorage.__proto__, handler)
+const define = new Storage()
+
+define.getItem('a')
+
